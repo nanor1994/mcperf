17a960604c61c9536f1ced1dda9775155c9c20f2
nanor1994 <74119454+nanor1994@users.noreply.github.com>
2022-12-13 17:34:54 +0200

    Update memcached-pt.py

--- memcached-pt.py
+++ memcached-pt.py
@@ -9,6 +9,8 @@ def main(argv):
     tids = os.popen("ps -p {} -o tid= -L | sort -n | tail -n +3 | head -{}".format(pid, worker_threads)).read().strip()
     cpu_id = 0
     for tid in tids.splitlines():
+        if cpu_id==10 
+            cpu_id=20
         tset = os.popen("taskset -pc {} {}".format(cpu_id, int(tid)))
         print(tset.read())
         cpu_id += 1
